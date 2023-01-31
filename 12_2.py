52a1830be84936a84601f95d7c9579b52c8f947c
XblazejX <75940221+XblazejX@users.noreply.github.com>
2023-01-31 09:36:14 +0100

    Add files via upload

---
+++ 12_2.py
@@ -0,0 +1,10 @@
+print("str37 cw 9.1")
+print("A)")
+for i in range(3,24,4):
+    print(i, end = ", ")
+print("\nB)")
+for i in range(9,4,-1):
+    print(i, end = ", ")
+print("\nC)")
+for i in range(12,-7,-3):
+    print(i, end = ", ")
